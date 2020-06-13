94121afb13e2fc45761474221fe838e9a3b63682
Hulklantern2 <hulklantern2v2@gmail.com>
2020-06-12 19:35:32 -0500

    Player movement controller, gives player ability to jump when spacebar is pressed.
    Applies a constant forward force to player and will allow player to move side to side when finished.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    //*******EXPIREMENTAL********\\
+    public float yForce = 30f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        rb.AddForce(sidewaysForce, 0, 0 * Time.deltaTime);
+        
+        if (Input.GetKeyDown("space"))
+        {
+            //*******EXPIREMENTAL********\\
+            rb.AddForce(0, yForce, 0 * Time.deltaTime);
+        }
+         
+    }
+}
+//go to Brackeys video script was deleted
